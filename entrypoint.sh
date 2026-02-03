fd0b05e5edcebe1f938895152fe0c7dfcb0e6fcf
Maksym Shmalko <maksym.shmalko@swarmer.tech>
2026-02-03 17:45:27 +0200

    fix(tag-action): strip prefix from pre_tag before semver operation

--- entrypoint.sh
+++ entrypoint.sh
@@ -213,10 +213,17 @@ then
         setOutput "tag" "$pre_tag"
         exit 0
     fi
+    # strip tag prefix from pre_tag for semver operations (same as current_tag)
+    if [ -z "$tagPrefix" ]
+    then
+      current_pre_tag=${pre_tag}
+    else
+      current_pre_tag="$(echo ${pre_tag}| sed "s;${tagPrefix};;g")"
+    fi
     # already a pre-release available, bump it
     if [[ "$pre_tag" =~ $new ]] && [[ "$pre_tag" =~ $suffix ]]
     then
-        new=${tagPrefix}$(semver -i prerelease "${pre_tag}" --preid "${suffix}")
+        new=${tagPrefix}$(semver -i prerelease "${current_pre_tag}" --preid "${suffix}")
         echo -e "Bumping ${suffix} pre-tag ${pre_tag}. New pre-tag ${new}"
     else
         new="${new}-${suffix}.0"
